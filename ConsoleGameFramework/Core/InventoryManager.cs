80cf62889104877d220220c56e8b5bb1e695acc4
HaJaeHeon <88596006+HaJaeHeon@users.noreply.github.com>
2026-07-08 15:58:04 +0900

    Update ShopScene / Update InventoryManager
    Update - ShopScene
    (아이템 구매할것인지 confirm하고 Log출력)
    Update - InventoryManager
    (ItemList 생성)

---
+++ ConsoleGameFramework/Core/InventoryManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class InventoryManager
+{
+    public enum Item
+    {
+        Sword,
+        Shield,
+        PowerPosion,
+        HealthPosion
+    }
+
+    private static InventoryManager instance = null;
+
+    public List<Item> EquipList = new List<Item>();
+    public List<Item> PosionList = new List<Item>();
+
+    public static InventoryManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new InventoryManager();
+
+            return instance;
+        }
+    }
+
+    public void Test()
+    {
+
+    }
+}
--- ConsoleGameFramework/Scenes/ShopScene.cs
+++ ConsoleGameFramework/Scenes/ShopScene.cs
@@ -3,21 +3,14 @@ using ConsoleGameFramework.UI;
 
 namespace ConsoleGameFramework_KR.Scenes
 {
-    public enum Item
-    {
-        Sword,
-        Shield,
-        PowerPosion,
-        HealthPosion
-    }
     public class ShopScene : SceneBase
     {
         private static readonly List<MenuOption> Menu = new List<MenuOption>
         { 
-        new MenuOption(1, "검을 구매합니다","검 - 3연속으로 커맨드를 정확히 입력하면 크리티컬 대미지를 입힙니다."),
-        new MenuOption(2, "방패를 구매합니다.", "방패 - 5연속으로 커맨드를 정확히 입력하면  HP를 회복합니다."),
-        new MenuOption(3, "공격력의 비약을 구매합니다.", "공격력의 비약 - 플레이어의 기본 공격력을 1 증가시킵니다."),
-        new MenuOption(4, "체력의 비약을 구매합니다..", "체력의 비약 - 플레이어의 최대 체력을 10 증가시킵니다."),
+        new MenuOption(1, "검 (30 G)","검 - 3연속으로 커맨드를 정확히 입력하면 크리티컬 대미지를 입힙니다."),
+        new MenuOption(2, "방패 (50 G)", "방패 - 5연속으로 커맨드를 정확히 입력하면  HP를 회복합니다."),
+        new MenuOption(3, "공격력의 비약 (300 G)", "공격력의 비약 - 플레이어의 기본 공격력을 1 증가시킵니다."),
+        new MenuOption(4, "체력의 비약 (350 G)", "체력의 비약 - 플레이어의 최대 체력을 10 증가시킵니다."),
         new MenuOption(9, "타이틀로", "첫 화면으로 돌아갑니다."),
         new MenuOption(0, "종료", "프로그램을 종료합니다.")
         };
@@ -41,24 +34,42 @@ namespace ConsoleGameFramework_KR.Scenes
         public override void HandleInput(GameContext context)
         {
             int choice = ConsoleUI.ReadMenuChoice(Menu);
+            InventoryManager iManager = InventoryManager.Instance;
+            bool purchase;
             //업적과 소지 골드를 확인해서 장비를 구매한다.
             //장비는 1개씩만 구매가 가능하고 비약들은 중첩해서 구매가 가능하다
             //비약들은 인벤토리 내로 들어가서 사용하면 수치가 올라간다.
             switch (choice)
             {
                 case 1:
-                    ConsoleUI.Confirm("정말로 구매하시겠습니까?");
-                    EnableMenuOption(Menu, choice);
+                    purchase = ConsoleUI.Confirm("정말로 구매하시겠습니까?");
+                    if (purchase)
+                    {
+                        EnableMenuOption(Menu, choice);
+                        PurchaseItem(Menu, choice);
+                    }
                     break;
                 case 2:
-                    ConsoleUI.Confirm("정말로 구매하시겠습니까?");
-                    EnableMenuOption(Menu, choice);
+                    purchase = ConsoleUI.Confirm("정말로 구매하시겠습니까?");
+                    if (purchase)
+                    {
+                        EnableMenuOption(Menu, choice);
+                        PurchaseItem(Menu, choice);
+                    }
                     break;
                 case 3:
-                    ConsoleUI.Confirm("정말로 구매하시겠습니까?");
+                    purchase = ConsoleUI.Confirm("정말로 구매하시겠습니까?");
+                    if (purchase)
+                    {
+                        PurchaseItem(Menu, choice);
+                    }
                     break;
                 case 4:
-                    ConsoleUI.Confirm("정말로 구매하시겠습니까?");
+                    purchase = ConsoleUI.Confirm("정말로 구매하시겠습니까?");
+                    if (purchase)
+                    {
+                        PurchaseItem(Menu, choice);
+                    }
                     break;
                 case 9:
                     GoTo(context, SceneKey.Title);
@@ -79,6 +90,16 @@ namespace ConsoleGameFramework_KR.Scenes
                 Menu[index] = Menu[index].ChangeEnable(false);
 
                 Console.WriteLine("검이 품절 처리되었습니다!");
+            }
+        }
+
+        public void PurchaseItem(IEnumerable<MenuOption> options, int num)
+        {
+            int index = Menu.FindIndex(option => option.Number == num);
+            if (index != -1)
+            {
+                GameManager manager = GameManager.Instance;
+                manager.Context.AddLog($"{Menu[num - 1].Label}을 구매했습니다.");
             }
         }
     }
